a7dd2af445eae02dc8d46ed07df2f59508a22ce4
3A432081陳紹田\admin <s707071127@gmail.com>
2017-10-25 17:54:06 +0800

    顯示已有的任務

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ Route::group(['middleware' => 'web'], function () {
      * 顯示所有任務
      */
     Route::get('/', function () {
-        //
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
     });
 
     /**
