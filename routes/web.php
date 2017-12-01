9eef027a94f76534ea470888da5a1dc28da0929a
3A432081 <s707071127@gmail.com>
2017-12-02 00:20:41 +0800

    重新輸入route

--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,38 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
-    return view('tasks');
-});
 use App\Task;
 use Illuminate\Http\Request;
-
-Route::group(['middleware' => 'web'], function () {
-
-    /**
-     * 顯示所有任務
-     */
-    Route::get('/', function () {
-        $tasks = Task::orderBy('created_at', 'asc')->get();
-
-        return view('tasks', [
-            'tasks' => $tasks
-        ]);
-    });
-
-    /**
-     * 增加新的任務
-     */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
-
-        return redirect('/');
-    });
-
-    /**
-     * 刪除任務
-     */
-    Route::delete('/task/{task}', function (Task $task) {
-        $task->delete();
-
-        return redirect('/');
-    });
+/**
+ * 顯示所有任務
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+/**
+ * 增加新的任務
+ */
+Route::post('/task', function (Request $request) {
+    ///驗證輸入
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+});
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+    return redirect('/');
 });
